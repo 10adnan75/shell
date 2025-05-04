0d4de3c9af253ee9ea760a816df281e0b57a7092
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-05-04 00:58:06 -0700

    The type builtin: builtins #EZ5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,6 +15,13 @@ public class Main {
                 System.exit(0);
             } else if (input.startsWith("echo")) {
                 System.out.println(input.substring(5));
+            } else if (input.startsWith("type")) {
+                String cmd = inputSplit[1];
+                if (cmd.equals("echo") || cmd.equals("type") || cmd.equals("exit")) {
+                    System.out.println(cmd + " is a shell builtin");
+                } else {
+                    System.out.println(cmd + ": not found");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }
