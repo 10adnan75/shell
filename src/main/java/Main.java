cebe3a0e36f7f65eff6c2a5cbd100dac44b4b151
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-05-04 01:15:22 -0700

    The type builtin: executable files #MG5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -11,7 +12,7 @@ public class Main {
 
             String[] inputSplit = input.split(" ");
             
-            if (input.equals("exit 0")) {
+            if (inputSplit[0].equals("exit")) {
                 System.exit(0);
             } else if (input.startsWith("echo")) {
                 System.out.println(input.substring(5));
@@ -20,7 +21,20 @@ public class Main {
                 if (cmd.equals("echo") || cmd.equals("type") || cmd.equals("exit")) {
                     System.out.println(cmd + " is a shell builtin");
                 } else {
-                    System.out.println(cmd + ": not found");
+                    String[] paths = System.getenv("PATH").split(":");
+                    boolean found = false;
+
+                    for (String dir : paths) {
+                        File file = new File(dir, cmd);
+                        if (file.exists() && file.canExecute()) {
+                            System.out.println(cmd + " is " + file.getAbsolutePath());
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println(cmd + ": not found");
+                    }
                 }
             } else {
                 System.out.println(input + ": command not found");
