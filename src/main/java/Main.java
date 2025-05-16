c8593f60a6b6e3c74c78b8e116e1c442c40ba5ef
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-05-16 01:51:58 -0700

    Single quotes #NI6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CommandHandler handler = new CommandHandler();
@@ -14,9 +19,23 @@ public class Main {
 
             if (input.isBlank()) continue;
 
-            String[] inputSplit = input.split(" ");
-            
+            String[] inputSplit = tokenizeInput(input);
             currentDirectory = handler.handleCommand(inputSplit, input, currentDirectory);
         }
+    }
+
+    public static String[] tokenizeInput(String input) {
+        List<String> tokens = new ArrayList<>();
+        Matcher matcher = Pattern.compile("'([^']*)'|\\S+").matcher(input);
+
+        while (matcher.find()) {
+            if (matcher.group(1) != null) {
+                tokens.add(matcher.group(1)); 
+            } else {
+                tokens.add(matcher.group()); 
+            }
+        }
+
+        return tokens.toArray(new String[0]);
     }
 }
