4486a6a1d6c8b6bb00436eb7be29c82c62789aa2
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-05-16 02:35:52 -0700

    Redirect stdout #JV1

--- src/main/java/builtins/EchoCommand.java
+++ src/main/java/builtins/EchoCommand.java
@@ -1,13 +1,18 @@
 package builtins;
 
 import java.nio.file.Path;
-import java.util.Arrays;
 
 public class EchoCommand implements Command {
     @Override
     public Path execute(String[] args, String rawInput, Path currentDirectory) {
         if (args.length > 1) {
-            System.out.println(String.join(" ", Arrays.copyOfRange(args, 1, args.length)));
+            StringBuilder output = new StringBuilder();
+            for (int i = 1; i < args.length; i++) {
+                output.append(args[i]);
+                if (i < args.length - 1)
+                    output.append(" ");
+            }
+            System.out.println(output.toString());
         } else {
             System.out.println();
         }
