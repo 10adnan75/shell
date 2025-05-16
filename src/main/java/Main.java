538be73c4b47e9dfe5f60af036e7084c6c2ab58b
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-05-16 01:58:59 -0700

    Single quotes #NI6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CommandHandler handler = new CommandHandler();
@@ -17,25 +16,24 @@ public class Main {
             System.out.print("$ ");
             String input = scanner.nextLine().trim();
 
-            if (input.isBlank()) continue;
+            if (input.isBlank())
+                continue;
 
-            String[] inputSplit = tokenizeInput(input);
+            List<String> tokensList = new ArrayList<>();
+            Matcher matcher = Pattern.compile("'([^']*)'|\\S+").matcher(input);
+            while (matcher.find()) {
+                if (matcher.group(1) != null) {
+                    tokensList.add(matcher.group(1));
+                } else {
+                    tokensList.add(matcher.group());
+                }
+            }
+
+            if (tokensList.isEmpty())
+                continue;
+
+            String[] inputSplit = tokensList.toArray(new String[0]);
             currentDirectory = handler.handleCommand(inputSplit, input, currentDirectory);
         }
-    }
-
-    public static String[] tokenizeInput(String input) {
-        List<String> tokens = new ArrayList<>();
-        Matcher matcher = Pattern.compile("'([^']*)'|\\S+").matcher(input);
-
-        while (matcher.find()) {
-            if (matcher.group(1) != null) {
-                tokens.add(matcher.group(1)); 
-            } else {
-                tokens.add(matcher.group()); 
-            }
-        }
-
-        return tokens.toArray(new String[0]);
     }
 }
--- src/main/java/builtins/EchoCommand.java
+++ src/main/java/builtins/EchoCommand.java
@@ -1,12 +1,13 @@
 package builtins;
 
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class EchoCommand implements Command {
     @Override
     public Path execute(String[] args, String rawInput, Path currentDirectory) {
-        if (rawInput.length() > 5) {
-            System.out.println(rawInput.substring(5));
+        if (args.length > 1) {
+            System.out.println(String.join(" ", Arrays.copyOfRange(args, 1, args.length)));
         } else {
             System.out.println();
         }
