df199caeb6f6291b938cac09c0eeb9cc0fe90591
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-05-22 13:54:46 -0700

    Append stdout #EL9

--- src/main/java/core/CommandHandler.java
+++ src/main/java/core/CommandHandler.java
@@ -74,21 +74,17 @@ public class CommandHandler {
         PrintStream originalErr = System.err;
         try {
             if (redirectFile != null && isBuiltin) {
-                System.setOut(new PrintStream(new FileOutputStream(redirectFile, false), true) {
+                System.setOut(new PrintStream(new FileOutputStream(redirectFile, isAppend), true) {
                     @Override
                     public void print(String s) {
-                        if (s.endsWith("$ ")) {
-                            super.print("\n" + s);
-                        } else {
+                        if (!s.endsWith("$ ")) {
                             super.print(s);
                         }
                     }
 
                     @Override
                     public void println(String s) {
-                        if (s.endsWith("$ ")) {
-                            super.print("\n" + s + "\n");
-                        } else {
+                        if (!s.endsWith("$ ")) {
                             super.print(s + "\n");
                         }
                     }
@@ -100,7 +96,7 @@ public class CommandHandler {
                 });
             }
             if (stderrRedirectFile != null && isBuiltin) {
-                System.setErr(new PrintStream(new FileOutputStream(stderrRedirectFile, false), true));
+                System.setErr(new PrintStream(new FileOutputStream(stderrRedirectFile, isAppend), true));
             }
 
             return cmd.execute(cmdTokensArray, rawCommand, currentDirectory);
