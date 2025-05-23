f566bbc48cec15aa01903f262b2b9af12a8f3336
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-05-22 17:26:55 -0700

    Append stdout #EL9

--- src/main/java/builtins/EchoCommand.java
+++ src/main/java/builtins/EchoCommand.java
@@ -5,18 +5,24 @@ import java.nio.file.Path;
 public class EchoCommand implements Command {
     @Override
     public Path execute(String[] args, String rawInput, Path currentDirectory) {
-        if (args.length > 1) {
-            StringBuilder output = new StringBuilder();
-            for (int i = 1; i < args.length; i++) {
-                output.append(args[i]);
-                if (i < args.length - 1) {
-                    output.append(" ");
-                }
-            }
-            System.out.print(output.toString());
-        } else {
-            System.out.print("");
+        if (args.length < 2) {
+            return currentDirectory;
         }
+
+        StringBuilder output = new StringBuilder();
+        for (int i = 1; i < args.length; i++) {
+            if (i > 1) {
+                output.append(" ");
+            }
+            String arg = args[i];
+            if ((arg.startsWith("\"") && arg.endsWith("\"")) ||
+                    (arg.startsWith("'") && arg.endsWith("'"))) {
+                arg = arg.substring(1, arg.length() - 1);
+            }
+            output.append(arg);
+        }
+
+        System.err.println(output.toString());
         return currentDirectory;
     }
 }
